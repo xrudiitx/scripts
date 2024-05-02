315a9164c01ab02f776efd0a75f3bad443299903
Rudi IT <159250213+xrudiitx@users.noreply.github.com>
2024-05-02 18:04:32 +0300

    Update init-tailscale-socks.sh

--- init-tailscale-socks.sh
+++ init-tailscale-socks.sh
@@ -3,5 +3,5 @@
 sudo tailscale down 2>/dev/null
 sudo chmod 644 /etc/default/tailscaled
 sudo sed -i 's/^FLAGS=".*"$/FLAGS="--tun=userspace-networking --socks5-server=0.0.0.0:1055 --outbound-http-proxy-listen=0.0.0.0:1055"/' /etc/default/tailscaled
-sudo tailscale up --accept-routes
 sudo systemctl restart tailscaled.service
+sudo tailscale up --accept-routes
